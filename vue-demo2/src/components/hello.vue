ba0b8a7b05f8ec149b46442e86854d5798965229
ldy1231 <34905459+ldy1231@users.noreply.github.com>
2018-07-24 18:15:10 +0800

    7.24_update

---
+++ vue-demo2/src/components/hello.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+      
+        <button type="button" @click="goHome">回到首页</button>
+        <button type="button" @click="gonnew">登陆页面</button>
+        <button type="button" @click="WeChat">微信页面</button>
+    </div>
+</template>
+<script>
+export default {
+    name:"hello",
+    data(){
+        return {
+            msg:"hello world!"
+        }
+    },
+    mounted(){
+
+    },
+    methods:{
+        goHome(){
+            this.$router.push({path:"/"})
+        },
+        gonnew(){
+            this.$router.push({path:"/hw"})
+        }
+        ,
+        WeChat(){
+            this.$router.push({path:"/wechat"})
+        }
+    }
+}
+</script>
+
